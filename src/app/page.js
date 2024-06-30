9487f11717dfd7bb2a049cfe8658491f3e65011c
SUDIP-69 <csudip2311@gmail.com>
2024-07-01 02:24:23 +0530

    commit

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,8 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 import { toast, Toaster } from "react-hot-toast";
+import logo from "./assets/baksish1.png";
+import Image from "next/image";
 
 export default function Home() {
   const [user, setuser] = useState("");
@@ -13,39 +15,54 @@ export default function Home() {
   const handlesubmit = async (event) => {
     event.preventDefault();
 
-    console.log(user, password);
+    // console.log(user, password);
 
     const {
-      data: { success },
+      data: { success,token },
     } = await axios.post("/api/login", { username: user, password });
+    // console.log(success);
     if (success) {
       setpassword("");
       setuser("");
       toast.success("successfully logged in");
+      localStorage.setItem('accessToken',token)
       setTimeout(() => {
         router.push("/ViewOrder");
       }, 1000);
     } else {
       setuser("");
 
-      toast.success("successfully logged in");
+      toast.error("successfully failed");
       setTimeout(() => {
-        router.push("/ViewOrder");
+        window.location.reload()
       }, 1000);
     }
   };
   return (
     <>
       <Toaster />
+      
       <header className="bg-[#4E0433] p-4 text-white font-bold tracking-widest text-center text-2xl">
-        LOGIN
+        <div>
+          <div className="flex justify-between items-center bg-[#FAFAFA] p-2 rounded-lg w-24 shadow-md shadow-[#AFAFAF]">
+            <Image alt="logo" width={100} height={10000} src={logo} />
+          </div>
+        </div>
       </header>
+
       <main>
         <form onSubmit={handlesubmit} className="max-w-xl mx-auto mt-40 p-6">
+          
+        <div className="flex font-extrabold tracking-widest text-2xl justify-center items-center -mt-10">
+          LOGIN
+        </div>
+        <hr className="h-[2px] mx-auto bg-[#4E0433] mb-10 rounded-2xl w-32" />
+
           <div className="relative z-0 w-full mb-5 group">
             <input
               type="text"
               name="floating_email"
+              value={user}
               onChange={(e) => setuser(e.target.value)}
               id="floating_email"
               className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-[#4E0433] peer"
@@ -61,6 +78,7 @@ export default function Home() {
           </div>
           <div className="relative z-0 w-full mb-5 group">
             <input
+            value={password}
               type="password"
               name="floating_password"
               onChange={(e) => setpassword(e.target.value)}

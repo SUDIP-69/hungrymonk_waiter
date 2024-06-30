53b9936ba1ce2647955df4ccaa9d6fe49a2e1832
SUDIP-69 <csudip2311@gmail.com>
2024-07-01 02:24:43 +0530

    commit

--- src/app/Components/Accordion.jsx
+++ src/app/Components/Accordion.jsx
@@ -1,18 +1,22 @@
 "use client"
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-function Accordion({ id, title, isOpen, onToggle, children }) {
+function Accordion({ id, title, isOpen, onToggle, children,status }) {
+  console.log(status);
     return (
 
-      <div className="border-b">
+      <div className="pb-5 ">
         <button
-          className="flex justify-between w-full p-4 text-left text-[#565556] text-lg font-semibold bg-[#ffffff]  hover:bg-gray-100"
+          className="flex justify-between w-full  shadow-lg p-5 text-left text-[#565556] text-lg font-semibold bg-[#ffffff]  hover:bg-gray-100"
           onClick={() => onToggle(id)}
         >
           {title}
+          <>
+          {/* status */}
+          </>
           <span>{isOpen ? <KeyboardArrowUpIcon/> :<KeyboardArrowDownIcon/> }</span>
         </button>
-        {isOpen && <div className="p-4">{children}</div>}
+        {isOpen && <div className="p-4 ">{children}</div>}
       </div>
     );
   }
